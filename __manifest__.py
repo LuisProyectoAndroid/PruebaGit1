6ebd909c36183ca81617beee331a46d014055e0f
Luis <luis.garcia@colegiomiralmonte.es>
2025-01-07 19:14:09 +0100

    primet commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Git',
+    'version': '1.0',
+    'category': '',
+    'summary': 'Git',
+    'description': '''
+        
+    ''',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/git_views.xml',
+        'views/menu_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
